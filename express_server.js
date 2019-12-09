1f7eac2b96400c51b73a5f220221d146812d512c
Tausif <engr.tausifkhan@gmail.com>
2019-12-09 22:31:42 +0000

    added the html body with the response for going to particular url

--- express_server.js
+++ express_server.js
@@ -9,8 +9,12 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-server.get("/", (req, res) => {
-  res.send(`<h1>Server says Hi! from port: ${port}</h1>`);
+server.get("/urls.json", (req, res) => {
+  //res.send(`<h1>Server says Hi! from port: ${port}</h1>`);
+  res.json(urlDatabase);
+});
+server.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 server.listen(port, () => {
